056049005fac0f03b3cc1a7a9f21c7495a0341b6
Nitish Kumar <nk045729@gmail.com>
2026-03-17 12:47:42 +0530

    update

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+let myObj = {
+  name: "Chai Aur Code",
+  age: 1,
+  isDeveloper: true
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-2xl'>Tailwind Test</h1>
+    {/* <Card channel="chaiAurCode" myArr={[1, 2, 3]}/>
+    <Card channel="chaiAurCode" myObj={myObj} /> */}
+
+    <Card username="Nitish" btnText="Subscribe"/>
+    <Card username="Chai Aur Code" btnText="Follow"/>
     </>
   )
 }
