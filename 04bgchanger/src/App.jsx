de9ed9400858859404d88b82afd43d922d5adbf5
Nitish Kumar <nk045729@gmail.com>
2026-03-17 15:10:40 +0530

    update

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("yellow")
+
+  return (
+    <>
+    <div className=''>
+
+    </div>
+      
+    </>
+  )
+}
+
+export default App
